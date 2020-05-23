1b11a65fe2ff24eb830780980c68b524cd76416b
luoqiang <lq9328@126.com>
2020-05-23 14:38:00 +0800

    feat: 添加客户端

---
+++ client/pages/user/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>aa</view>
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component';
+
+@Component({})
+export default class Login extends Vue {
+  onLoad() {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
