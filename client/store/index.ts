99377d2d9abba7e3e4de95c8babf0cd10e8a5616
罗强 <lq9328@126.com>
2020-10-21 17:03:25 +0800

    feat: 增加线上配置

--- client/store/index.ts
+++ client/store/index.ts
@@ -8,7 +8,7 @@ const modulesFiles = require.context('./modules', false, /\.ts$/)
 
 // you do not need `import app from './modules/app'`
 // it will auto require all vuex module from modules file
-const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+const modules = modulesFiles.keys().reduce((modules: { [x: string]: any; }, modulePath: string) => {
   // set './app.js' => 'app'
   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
   const value = modulesFiles(modulePath)
