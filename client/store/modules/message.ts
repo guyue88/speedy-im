4151a5b73a1746edb1a2b9463bf8f79982edca79
罗强 <lq9328@126.com>
2020-10-26 14:23:37 +0800

    feat: 增加消息自动功能

--- client/store/modules/message.ts
+++ client/store/modules/message.ts
@@ -4,15 +4,18 @@ import { MessageRecord } from '../../interface/entity';
 
 export interface State {
   list: Record<number, MessageRecord[]>;
+  lastHashIdMap: Record<number, string>; // 用于设置滚动到底部的hash id
 }
 
 const state: State = {
   list: {},
+  lastHashIdMap: {},
 };
 
 const mutations = {
   SET_USER_MESSAGES(state: State, { messages }: { messages: MessageRecord[] }) {
     const list = {...state.list};
+    const lastHashIdMap = {...state.lastHashIdMap};
     messages.forEach(item => {
       const { dist_id, user_id, is_owner } = item;
       // 自己发的消息，friend_id是dist_id，否则则是user_id
@@ -23,10 +26,12 @@ const mutations = {
       } else {
         !msg.find(l => l.hash === item.hash) && msg.push(item);
       }
+      lastHashIdMap[friend_id] = item.hash;
     });
     state.list = list;
+    state.lastHashIdMap = lastHashIdMap;
   },
-  UPDATE_MESSAGE(state: State, { messages }: { messages: { id: number, hash: string, friend_id: number } }) {
+  UPDATE_MESSAGE_ID(state: State, { messages }: { messages: { id: number, hash: string, friend_id: number } }) {
     const list = {...state.list};
     const { id, hash, friend_id } = messages;
     const msg = list[friend_id];
@@ -59,7 +64,7 @@ const actions = {
   },
   updateMessage({ commit }: ActionContext<State, any>, payload: { messages: { id: number, hash: string, friend_id: number } }) {
     const { messages } = payload;
-    commit('UPDATE_MESSAGE', { messages });
+    commit('UPDATE_MESSAGE_ID', { messages });
   }
 };
 
